fbd3ef2ac60be88228af9acfaf6849f6ca7c98f8
John Shortt <60323583+jsCarleton@users.noreply.github.com>
2021-04-15 13:42:34 -0400

    Organized code

--- elevator.py
+++ elevator.py
@@ -32,93 +32,35 @@ def get_bimodal_dist(n_floors, peak1, peak2, var):
     p = p1 + 4*p2/3
     return p/p.sum()
 
+# print sample results for distributions
+floors_list = [8, 12, 16, 20]
 print("Gaussian distributions")
-floors = 8
-p = get_normal_dist(floors)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
-
-floors = 12
-p = get_normal_dist(floors)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
-
-floors = 16
-p = get_normal_dist(floors)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
-
-floors = 20
-p = get_normal_dist(floors)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
+for floors in floors_list:
+    p = get_normal_dist(floors)
+    print(floors, " floors:", p, "sum =",p.sum())
+    _ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
 
 print("Bimodal distributions")
-floors = 8
-p = get_bimodal_dist(floors, 2, 7, 1)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
+bimodal_params = [[8, 2, 7, 1], [12, 3, 9, 2], [16, 3, 13, 3], [20, 4, 15, 3]]
+for b in bimodal_params:
+    p = get_bimodal_dist(b[0], b[1], b[2], b[3])
+    print(b[0], " floors:", p, "sum =",p.sum())
+    _ = plt.hist(np.random.choice(np.arange(1, b[0]+1), size = 10000, p=p), bins=b[0])
 
+print("Exponential distributions")
+for floors in floors_list:
+    p = get_exp_dist(floors)
+    print(floors, " floors:", p, "sum =",p.sum())
+    _ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
 
-floors = 12
-p = get_bimodal_dist(floors, 3, 9, 2)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
+print("Reverse exponential distributions")
+for floors in floors_list:
+    p = get_rev_exp_dist(floors)
+    print(floors, " floors:", p, "sum =",p.sum())
+    _ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
+print()
 
-
-floors = 16
-p = get_bimodal_dist(floors, 3, 13, 3)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
-
-
-floors = 20
-p = get_bimodal_dist(floors, 4, 15, 3)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
-
-
-floors = 20
-p = get_exp_dist(floors)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
-
-floors = 16
-p = get_exp_dist(floors)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
-
-floors = 12
-p = get_exp_dist(floors)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
-
-floors = 8
-p = get_exp_dist(floors)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
-
-floors = 20
-p = get_rev_exp_dist(floors)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
-
-floors = 16
-p = get_rev_exp_dist(floors)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
-
-floors = 12
-p = get_rev_exp_dist(floors)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
-
-floors = 8
-p = get_rev_exp_dist(floors)
-print(floors, " floors:", p, "sum =",p.sum())
-_ = plt.hist(np.random.choice(np.arange(1, floors+1), size = 10000, p=p), bins=floors)
-
-
+# function to return a random floor given a probability distribution
 def get_random_floor(call_probs):
     r = random.uniform(0, 1)
     sum = 0.0
@@ -126,8 +68,7 @@ def get_random_floor(call_probs):
         if r < sum:
             return floor
         sum += call_probs[floor]
-    return len(call_probs)
-    
+    return len(call_probs)    
 
 # function to find the 2 best floors to park an elevator
 # at given distribution of calls at the floor
@@ -149,26 +90,22 @@ def get_best_floors(call_probs, iterations):
                 floor_2 = j
     return floor_1,floor_2
 
-print("Best floors for normal distribution on 8 floors", get_best_floors(get_normal_dist(8), 1000))
-print("Best floors for normal distribution on 12 floors", get_best_floors(get_normal_dist(12), 1000))
-print("Best floors for normal distribution on 16 floors", get_best_floors(get_normal_dist(16), 1000))
-print("Best floors for normal distribution on 20 floors", get_best_floors(get_normal_dist(20), 1000))
+print("Best waiting floors")
+print("-------------------")
+for floors in floors_list:
+    print("Normal distribution on", floors, "floors:", get_best_floors(get_normal_dist(floors), 1000))
 
-print("Best floors for bimodal distribution on 8 floors", get_best_floors(get_bimodal_dist(8, 2, 7, 1), 1000))
-print("Best floors for bimodal distribution on 12 floors", get_best_floors(get_bimodal_dist(12, 3, 9, 2), 1000))
-print("Best floors for bimodal distribution on 16 floors", get_best_floors(get_bimodal_dist(16, 3, 13, 3), 1000))
-print("Best floors for bimodal distribution on 20 floors", get_best_floors(get_bimodal_dist(20, 4, 15, 3), 1000))
+for b in bimodal_params:
+    print("Bimodal distribution on", b[0], "floors:", get_best_floors(get_bimodal_dist(b[0], b[1], b[2], b[3]), 1000))
 
-print("Best floors for exponential distribution on 8 floors", get_best_floors(get_exp_dist(8), 1000))
-print("Best floors for exponential distribution on 12 floors", get_best_floors(get_exp_dist(12), 1000))
-print("Best floors for exponential distribution on 16 floors", get_best_floors(get_exp_dist(16), 1000))
-print("Best floors for exponential distribution on 20 floors", get_best_floors(get_exp_dist(20), 1000))
+for floors in floors_list:
+    print("Exponential distribution on", floors, "floors:", get_best_floors(get_exp_dist(floors), 1000))
 
-print("Best floors for reverse exponential distribution on 8 floors", get_best_floors(get_rev_exp_dist(8), 1000))
-print("Best floors for reverse exponential distribution on 12 floors", get_best_floors(get_rev_exp_dist(12), 1000))
-print("Best floors for reverse exponential distribution on 16 floors", get_best_floors(get_rev_exp_dist(16), 1000))
-print("Best floors for reverse exponential distribution on 20 floors", get_best_floors(get_rev_exp_dist(20), 1000))
+for floors in floors_list:
+    print("Reverse exponential distribution on", floors, "floors:", get_best_floors(get_rev_exp_dist(floors), 1000))
+print()
 
+# a function that returns the average distance travelled when the distribution is known
 def opaque_model(call_dist, iterations):
     distance = 0
     el_1, el_2 = get_best_floors(call_dist, iterations)
@@ -177,25 +114,20 @@ def opaque_model(call_dist, iterations):
         distance += min(abs(el_1 - f), abs(el_2 - f))
     return distance/iterations
 
-print(opaque_model(get_normal_dist(8), 1000))
-print(opaque_model(get_normal_dist(12), 1000))
-print(opaque_model(get_normal_dist(16), 1000))
-print(opaque_model(get_normal_dist(20), 1000))
+print("AWT for opaque models")
+print("---------------------")
+for floors in floors_list:
+    print("Normal distribution,", floors, "floors:", opaque_model(get_normal_dist(floors), 1000))
 
-print(opaque_model(get_bimodal_dist(8, 2, 7, 1), 1000))
-print(opaque_model(get_bimodal_dist(12, 3, 9, 2), 1000))
-print(opaque_model(get_bimodal_dist(16, 3, 13, 3), 1000))
-print(opaque_model(get_bimodal_dist(20, 4, 15, 3), 1000))
+for b in bimodal_params:
+    print("Bimodal distribution,", b[0], "floors:", opaque_model(get_bimodal_dist(b[0], b[1], b[2], b[3]), 1000))
 
-print(opaque_model(get_exp_dist(8), 1000))
-print(opaque_model(get_exp_dist(12), 1000))
-print(opaque_model(get_exp_dist(16), 1000))
-print(opaque_model(get_exp_dist(20), 1000))
+for floors in floors_list:
+    print("Exponential distribution,", floors, "floors:", opaque_model(get_exp_dist(floors), 1000))
 
-print(opaque_model(get_rev_exp_dist(8), 10000))
-print(opaque_model(get_rev_exp_dist(12), 10000))
-print(opaque_model(get_rev_exp_dist(16), 10000))
-print(opaque_model(get_rev_exp_dist(20), 10000))
+for floors in floors_list:
+    print("Reverse exponential distribution,", floors, "floors:", opaque_model(get_rev_exp_dist(floors), 1000))
+print()
 
 class Environment():
     
@@ -295,18 +227,42 @@ class ElevatorBank():
             floor = get_random_floor(self.call_probs) - 1
             calls[floor] += 1
             self.elevators[self.get_best_elevator(floor)].carry_to(floor)
-        print(np.array(calls)/self.call_probs)
+#        print(np.array(calls)/self.call_probs)
 #            self.print_state(floor)
 #        print(self.elevators[0].floor, self.elevators[0].floor_probs)
 #        print(self.elevators[1].floor, self.elevators[1].floor_probs)
 
-total = 0
-for i in range(1):
-    bank = ElevatorBank(1, 20, get_normal_dist(20), 0.1)
-    bank.simulate(10000000)
-    total += bank.env.total_distance/1000
-print(total/10000)
+print("AWT for L-RI models")
+print("-------------------")
+for floors in floors_list:
+    bank = ElevatorBank(1, floors, get_normal_dist(floors), 0.1)
+    bank.simulate(200000)
+    print("Normal distribution,", floors, "floors:", bank.env.total_distance/200000)
+
+for b in bimodal_params:
+    bank = ElevatorBank(1, b[0], get_bimodal_dist(b[0], b[1], b[2], b[3]), 0.1)
+    bank.simulate(200000)
+    print("Bimodal distribution,", b[0], "floors:", bank.env.total_distance/200000)
+
+for floors in floors_list:
+    bank = ElevatorBank(1, floors, get_exp_dist(floors), 0.1)
+    bank.simulate(200000)
+    print("Exponential distribution,", floors, "floors:", bank.env.total_distance/200000)
+
+for floors in floors_list:
+    bank = ElevatorBank(1, floors, get_rev_exp_dist(floors), 0.1)
+    bank.simulate(200000)
+    print("Reverse exponential distribution,", floors, "floors:", bank.env.total_distance/200000)
+print()
+
+
+#total = 0
+#for i in range(1):
+#    bank = ElevatorBank(1, 20, get_normal_dist(20), 0.1)
+#    bank.simulate(10000000)
+#    total += bank.env.total_distance/1000
+#print(total/10000)
 #bank.env.print_distance()
 
-print(bank.elevators[0].floor, bank.elevators[0].floor_probs)
-print(bank.elevators[1].floor, bank.elevators[1].floor_probs)
+#print(bank.elevators[0].floor, bank.elevators[0].floor_probs)
+#print(bank.elevators[1].floor, bank.elevators[1].floor_probs)
